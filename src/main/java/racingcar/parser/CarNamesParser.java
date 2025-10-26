0c4069631118198e0c507ecb60054202c7e732c2
jihoo2002 <yolohoo@naver.com>
2025-10-27 03:18:43 +0900

    feat : 사용자 입력 자동차 이름 파싱 및 유효성 검사

---
+++ src/main/java/racingcar/parser/CarNamesParser.java
@@ -0,0 +1,30 @@
+package racingcar.parser;
+
+import racingcar.util.ErrorMessage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CarNamesParser {
+
+    public static List<String> parseCarNames(String input) {
+        throwIfCarsListIsNullOrEmpty(input);
+
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .peek(CarNamesParser::throwIfCarNameEmpty)
+                .toList();
+    }
+
+    private static void throwIfCarNameEmpty(String car) {
+        if (car.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_CAR_NAME.getMessage());
+        }
+    }
+
+    private static void throwIfCarsListIsNullOrEmpty(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_CAR_LIST.getMessage());
+        }
+    }
+}
