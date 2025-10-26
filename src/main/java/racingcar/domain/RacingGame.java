81702f05c5d387b68a631cb442c7606d943a4f76
jihoo2002 <yolohoo@naver.com>
2025-10-27 03:15:34 +0900

    feat : 레이싱 게임에서 자동차 이동 로직 & 랜덤 값 적용

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,21 @@
+package racingcar.domain;
+
+import racingcar.util.RandomUtil;
+
+public class RacingGame {
+
+    private final Cars cars;
+    private final RandomUtil randomUtil;
+
+    public RacingGame(Cars cars, RandomUtil randomUtil) {
+        this.cars = cars;
+        this.randomUtil = randomUtil;
+    }
+
+    public void race() {
+        for (Car car : cars.getCars()) {
+            int randomNum = randomUtil.generateRandomNumber();
+            car.move(randomNum);
+        }
+    }
+}
