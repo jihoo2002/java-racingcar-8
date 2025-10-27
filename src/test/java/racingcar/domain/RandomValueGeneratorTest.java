10c04dadb6e41e9dff1ad5133fdb1f0122a3b084
jihoo2002 <yolohoo@naver.com>
2025-10-27 11:07:58 +0900

    feat : displayName 수정

--- src/test/java/racingcar/domain/RandomValueGeneratorTest.java
+++ src/test/java/racingcar/domain/RandomValueGeneratorTest.java
@@ -12,7 +12,7 @@ class RandomValueGeneratorTest {
     private final RandomValueGenerator generator = new RandomValueGenerator();
 
     @Test
-    @DisplayName("랜덤값을 생성하면 10 내의 범위에 있는지 검증한다.")
+    @DisplayName("랜덤값을 생성하면 10 내의 범위에 있는지 검증")
     void 랜덤값이_범위_내에_있는지_검증() {
         int randomNumber = generator.generateRandomNumber();
 
