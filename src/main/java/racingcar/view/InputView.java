8bca72bad8ab1333975b2be3739a668ab8301fbf
jihoo2002 <yolohoo@naver.com>
2025-10-27 11:46:06 +0900

    refactor : console.close 명시적 삭제

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,15 +6,11 @@ public class InputView {
 
     public static String getCars() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String carNamesInput = Console.readLine();
-        Console.close();
-        return carNamesInput;
+        return Console.readLine();
     }
 
     public static String getRaceRounds() {
         System.out.println("시도할 회수는 몇회인가요?");
-        String roundsInput = Console.readLine();
-        Console.close();
-        return roundsInput;
+        return Console.readLine();
     }
 }
